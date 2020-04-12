e5ba4f53c2575e1d7554d09447e89a7198758a32
Treska <georgios.treska@sap.com>
2020-04-12 22:33:31 +0200

    fix inspector issue

--- app/scripts/modules/injected/message.js
+++ app/scripts/modules/injected/message.js
@@ -1,15 +1,38 @@
 'use strict';
 
+//Fallback if circular ref is detected
+function simpleStringify (object){
+    var simpleObject = {};
+    for (var prop in object ){
+        if (!object.hasOwnProperty(prop)){
+            continue;
+        }
+        if (typeof(object[prop]) == 'object'){
+            continue;
+        }
+        if (typeof(object[prop]) == 'function'){
+            continue;
+        }
+        simpleObject[prop] = object[prop];
+    }
+    return JSON.stringify(simpleObject); // returns cleaned up JSON
+};
 module.exports = {
     /**
      * Send message to content script.
      * @param {Object} object
      */
     send: function (object) {
-        var message = {
-            detail: JSON.parse(JSON.stringify(object))
-        };
-
-        document.dispatchEvent(new CustomEvent('ui5-communication-with-content-script', message));
+        try {
+            var message = {
+                detail: JSON.parse(JSON.stringify(object))
+            };
+            
+        } catch (error) {
+            message = {
+                detail: JSON.parse(simpleStringify(object))
+            };
+        }
+        document.dispatchEvent(new CustomEvent('ui5-communication-with-content-script', message));   
     }
 };
