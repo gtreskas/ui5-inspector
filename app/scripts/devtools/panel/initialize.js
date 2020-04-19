5eaf4d8782f777a261dad0cdfe17109210b1fadd
Treska <georgios.treska@sap.com>
2020-04-19 13:12:19 +0200

    add non ui5 vyper recorder panel

--- app/scripts/devtools/panel/initialize.js
+++ app/scripts/devtools/panel/initialize.js
@@ -3,6 +3,18 @@
 // Create a port with background page for continuous message communication
 var port = chrome.extension.connect({name: 'devtools-initialize-tabId-' + chrome.devtools.inspectedWindow.tabId});
 
+// The function below is executed in the context of the inspected page.
+var page_getProperties = function() {
+    var data = window.jQuery && $0 ? jQuery.data($0) : {};
+    // Make a shallow copy with a null prototype, so that sidebar does not
+    // expose prototype.
+    var props = Object.getOwnPropertyNames(data);
+    var copy = { __proto__: null };
+    for (var i = 0; i < props.length; ++i)
+    copy[props[i]] = data[props[i]];
+    return copy;
+}
+
 /**
  * Find the ID of the nearest UI5 control from the current selected element in Chrome elements panel.
  * @param {string} selectedElement - The ID of the selected element in Chrome elements panel
@@ -33,7 +45,17 @@ chrome.devtools.panels.create('UI5', '/images/icon-128.png', '/html/panel/ui5/in
     });
 });
 
+chrome.devtools.panels.elements.createSidebarPane(
+    "Vyper Non-UI5 Recorder",
+    function(sidebar) {
+        sidebar.setHeight('200px');
+        function updateElementProperties() { sidebar.setExpression("(" + page_getProperties.toString() + ")()"); }
+        updateElementProperties();
+        chrome.devtools.panels.elements.onSelectionChanged.addListener(updateElementProperties);
+});
+
 chrome.devtools.panels.elements.onSelectionChanged.addListener(function () {
+    
     // To get the selected element in Chrome elements panel, is needed to use eval and call the function with $0 parameter
     var getNearestUI5Control = _getNearestUI5ControlID.toString() + '_getNearestUI5ControlID($0);';
 
